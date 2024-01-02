ae09355602177c03ede20ee1b495a0fd01cdc08b
hamzajaada <hamzajaada22@gmail.com>
2024-01-02 15:26:12 +0100

    omor mzbota

--- frontend/src/Components/3-LoginForm/LoginForm.jsx
+++ frontend/src/Components/3-LoginForm/LoginForm.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import './LoginForm.css'
+import { useEffect ,useState } from 'react';
 
 const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Ajoutez ici la logique pour traiter la soumission du formulaire (connexion, appel API, etc.)
+  };
+
   return (
-    <div>
-      
+    <div className="login-form-container">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Nom d'utilisateur:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+
+        <label htmlFor="password">Mot de passe:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <button type="submit">Connexion</button>
+      </form>
     </div>
   );
 }
