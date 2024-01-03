60e5c94b0ce64fd7c0e8d04cc47d788435928dc9
ElgallatiAbdelilah <abdelilah.elgallati6@gmail.com>
2024-01-03 19:04:16 +0100

    pushed by abdelilah

--- Backend/app.js
+++ Backend/app.js
@@ -1,34 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 // const url = "mongodb://localhost:27017/DSREACT"
-const url = "mongodb://127.0.0.1:27017/DSREACT"
-const session = require("express-session")
+const url = "mongodb://127.0.0.1:27017/DSREACT";
+const session = require("express-session");
 const app = express();
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-const apiRouter = require("./routes/ApiRoute")
+const apiRouter = require("./routes/ApiRoute");
+
 app.use(
-    session({
-        secret : "Secret",
-        resave : false,
-        saveUninitialized :true,
-    })
+  session({
+    secret: "Secret",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
-// const webRouter = require("./routes/WebRoutes")
-// app.use(express.static('public'))
 
-// app.set("view engine", "pug");
-// app.set("views", "./views");
-// var methodOverride = require('method-override')
-// app.use(methodOverride('_method'))
-// // app.use(express.urlencoded({extended:true}));
-mongoose.connect(url).then(()=>{
+mongoose
+  .connect(url)
+  .then(() => {
     console.log("connecting to MONGODB");
-}).catch((err)=>{console.log(err);})
-app.use("/api",apiRouter);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use("/api", apiRouter);
 
-app.listen(3000,()=>{
-    console.log("Running!!");
-})
+app.listen(3000, () => {
+  console.log("Running!!");
+});
